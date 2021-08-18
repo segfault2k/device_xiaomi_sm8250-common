016d4c49b5be7dfd0af745b64436f1486ddcddaa
segfault2k <sebastian.sariego.benitez@gmail.com>
2021-08-17 22:09:06 -0400

    sm8250-common: Drop AOSP lights AIDL and switch to AOSP light HIDL
    * For some reason Corvus don't boot with lights AIDL

--- kona.mk
+++ kona.mk
@@ -250,7 +250,9 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.lights-service.xiaomi_kona
+    lights.kona \
+    android.hardware.light@2.0-impl \
+    android.hardware.light@2.0-service
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
